51f175a5e99104a174e35cfbdaf0019fd457f6e7
c8501024 <dominik.mair@uibk.ac.at>
2020-06-19 11:26:12 +0200

    initial commit minimal example

---
+++ .history/src/app/app.component_20200616102747.ts
@@ -0,0 +1,55 @@
+import { Component } from "@angular/core";
+
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+
+import {
+  BackgroundGeolocation,
+  BackgroundGeolocationLocationProvider,
+  BackgroundGeolocationAccuracy,
+} from "@ionic-native/background-geolocation/ngx";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private backgroundGeolocation: BackgroundGeolocation
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+
+      this.backgroundGeolocation
+        .configure({
+          locationProvider: BackgroundGeolocationLocationProvider.RAW_PROVIDER,
+          desiredAccuracy: BackgroundGeolocationAccuracy.HIGH,
+          stationaryRadius: 5,
+          startForeground: true,
+          distanceFilter: 0,
+          notificationTitle: "Background tracking",
+          notificationText: "fully enabled",
+          fastestInterval: 1 * 500, // 0.5 seconds
+          interval: 2 * 1000, // 1 seconds
+          debug: true,
+          stopOnTerminate: false,
+          pauseLocationUpdates: false,
+          saveBatteryOnBackground: false,
+          startOnBoot: true,
+        })
+        .then(() => {
+          this.backgroundGeolocation.start();
+        });
+    });
+  }
+}
